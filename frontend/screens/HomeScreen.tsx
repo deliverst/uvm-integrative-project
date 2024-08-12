6ae72901d0f75f47f142e8a84e29c344c0d72f80
Javier Valles <deliverst@icloud.com>
2024-08-12 01:33:20 -0600

    run with screen views

---
+++ frontend/screens/HomeScreen.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../App';
+
+type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
+
+type Props = {
+  navigation: HomeScreenNavigationProp;
+};
+
+const HomeScreen: React.FC<Props> = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
